54248c09edf1dbfb0bc0364ed75619dadb08eda6
斎藤正大 <saima.ptw88@gmail.com>
2020-11-03 14:58:47 +0900

    r

--- db/migrate/20201101060141_devise_token_auth_create_users.rb
+++ db/migrate/20201101060141_devise_token_auth_create_users.rb
@@ -1,7 +1,7 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
   def change
-    # create_table(:users) do |t|
-    create_table :users, bulk: true do |t|
+    create_table(:users) do |t|
+      # create_table :users, bulk: true do |t|
       ## Required
       t.string :provider, null: false, default: "email"
       t.string :uid, null: false, default: ""
@@ -38,10 +38,10 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    add_index users: :email,                unique: true
-    add_index users: [:uid, :provider],     unique: true
-    add_index users: :reset_password_token, unique: true
-    add_index users: :confirmation_token,   unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, [:uid, :provider],     unique: true
+    add_index :users, :reset_password_token, unique: true
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,       unique: true
   end
 end
